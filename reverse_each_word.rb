872b87becf5dca7fa7ba928e65100d7dcfd669e8
Reed Roffis <reedroffis18@gmail.com>
2020-12-03 06:54:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,8 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split(' ')
   output_string = ''
   sentence_array.collect do |word|
-      reversed_word = word.reverse
-      output_string << reversed_word + " " 
-    end
+    reversed_word = word.reverse
+    output_string << reversed_word + " " 
   end
   output_string.delete_suffix(' ')
 end 
