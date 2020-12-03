0d6e32f90138e123ed2e5c532677936887a06631
Reed Roffis <reedroffis18@gmail.com>
2020-12-03 06:54:40 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,9 @@
 def reverse_each_word(sentence) 
   sentence_array = sentence.split(' ')
-  output_string = nil 
-  sentence_array.each_with_index do |word, index|
-    if index == array.length - 1 
-       reversed_word = word.reverse
-       output_string << reversed_word 
-    else 
-      reversed_word = word.reverse
-      output_string << reversed_word + " " 
-    end
-  output_string
+  output_string = ''
+  sentence_array.collect do |word|
+    reversed_word = word.reverse
+    output_string << reversed_word + " " 
+  end
+  output_string.delete_suffix(' ')
 end 
